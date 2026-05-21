9c3fc409d43745c135912439fca5a10be04b2094
vikassulakhe <144027369+vikassulakhe@users.noreply.github.com>
2026-05-21 09:12:17 +0530

    Create config.py

---
+++ Stock_analyzer/config.py
@@ -0,0 +1,9 @@
+# ==============================
+# config.py
+# ==============================
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
